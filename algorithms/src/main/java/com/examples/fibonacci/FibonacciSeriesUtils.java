c27a3b43b36046f8ff728647151419052c841d6f
leonx7 <leonx7@tut.by>
2020-07-04 23:48:17 +0300

    feat: add algorithms to compute the nth Fibonacci term

---
+++ algorithms/src/main/java/com/examples/fibonacci/FibonacciSeriesUtils.java
@@ -0,0 +1,55 @@
+package com.examples.fibonacci;
+
+public class FibonacciSeriesUtils {
+
+    public static int nthFibonacciTermRecursiveMethod(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return (nthFibonacciTermRecursiveMethod(n - 1) + nthFibonacciTermRecursiveMethod(n - 2));
+    }
+
+    public static int nthFibonacciTermCashingMethodUtil(int n, int[] f) {
+
+        if (f[n] == -1) {
+            f[n] = (nthFibonacciTermCashingMethodUtil(n - 1, f) + nthFibonacciTermCashingMethodUtil(n - 2, f));
+        }
+        return f[n];
+    }
+
+    public static int nthFibonacciTermCashingMethod(int n) {
+        int[] f = new int[n + 1];
+        f[0] = 0;
+        f[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            f[i] = -1;
+        }
+        return nthFibonacciTermCashingMethodUtil(n, f);
+    }
+
+    public static int nthFibonacciTermIterativeMethod(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int n0 = 0, n1 = 1;
+        int tempNthTerm = 0;
+        for (int i = 2; i <= n; i++) {
+            tempNthTerm = n0 + n1;
+            n0 = n1;
+            n1 = tempNthTerm;
+        }
+        return tempNthTerm;
+    }
+
+    public static int nthFibonacciTermUsingBinetsFormula(int n) {
+        double squareRoutOf5 = Math.sqrt(5);
+        double phi = (1 + squareRoutOf5) / 2;
+        int nthTerm = (int) ((Math.pow(phi, n) - Math.pow(-phi, -n)) / squareRoutOf5);
+        return nthTerm;
+    }
+
+    public static void main(String[] args) {
+        int i = nthFibonacciTermIterativeMethod(7);
+        System.out.println(i);
+    }
+}
---
+++ algorithms/src/test/java/com/examples/fibonacci/FibonacciSeriesUtilsTest.java
@@ -0,0 +1,36 @@
+package com.examples.fibonacci;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FibonacciSeriesUtilsTest {
+
+    @Test
+    void nthFibonacciTermRecursiveMethod() {
+        int term = 10;
+        int expectedValue = 55;
+        assertEquals(FibonacciSeriesUtils.nthFibonacciTermRecursiveMethod(term), expectedValue);
+    }
+
+    @Test
+    void nthFibonacciTermCashingMethod() {
+        int term = 10;
+        int expectedValue = 55;
+        assertEquals(FibonacciSeriesUtils.nthFibonacciTermCashingMethod(term), expectedValue);
+    }
+
+    @Test
+    void nthFibonacciTermIterativeMethod() {
+        int term = 10;
+        int expectedValue = 55;
+        assertEquals(FibonacciSeriesUtils.nthFibonacciTermIterativeMethod(term), expectedValue);
+    }
+
+    @Test
+    void nthFibonacciTermUsingBinetsFormula() {
+        int term = 10;
+        int expectedValue = 55;
+        assertEquals(FibonacciSeriesUtils.nthFibonacciTermUsingBinetsFormula(term), expectedValue);
+    }
+}
